72fafac78625afbb5284bb0b38c276b20d7d3478
RafaelKramerDaSilvaDev <rafaelkramerdasilva.dev@gmail.com>
2024-12-07 15:54:39 -0300

    feat: create card hover effect

---
+++ src/pages/Characters/components/Details/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Details = styled.div`
+  background-color: #000000;
+  width: 100%;
+  height: 100%;
+`;
--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import charactersService from "../../services/characters/characters-service";
 import { Aside } from "./components/Aside";
+import { Details } from "./components/Details";
 import * as S from "./styles";
 
 export const CharactersPage = () => {
@@ -12,6 +13,8 @@ export const CharactersPage = () => {
   return (
     <S.Section>
       <Aside charactersData={charactersData} />
+
+      <Details />
 
       <Aside charactersData={charactersData} />
     </S.Section>
